3c6eb53bf46e459330d4c2850017d43d44d8c1a5
Carlos Martinez <curlosmart@gmail.com>
2022-03-05 08:41:49 -0500

    Add location page and countries data

---
+++ components/LocationCountries.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const LocationCountries = () => {
+  const COUNTRIES = [
+    {
+      name: 'Canada',
+      address: {
+        title: 'Designo Central Office',
+        street: '3886 Wellington Street',
+        state: 'Toronto, Ontario M9C 3J5'
+      },
+      contact: {
+        phoneNumber: '+1 253-863-8967',
+        email: 'contact@designo.co'
+      },
+      image: 'image-map-canada.png'
+    },
+    {
+      name: 'Australia',
+      address: {
+        title: 'Designo AU Office',
+        street: '19 Balonne Street',
+        state: 'New South Wales 2443'
+      },
+      contact: {
+        phoneNumber: '(02) 6720 9092',
+        email: 'contact@designo.au'
+      },
+      image: 'image-map-australia.png'
+    },
+    {
+      name: 'United Kingdom',
+      address: {
+        title: 'Designo UK Office',
+        street: '13  Colorado Way',
+        state: 'Rhyd-y-fro SA8 9GA'
+      },
+      contact: {
+        phoneNumber: '078 3115 1400',
+        email: 'contact@designo.uk'
+      },
+      image: 'image-map-united-kingdom.png'
+    }
+  ]
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LocationCountries
